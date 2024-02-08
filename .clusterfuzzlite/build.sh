28c3cece0eeb458fe55698195a5686832525485c
Lucas TESSON <lucastesson@protonmail.com>
2024-02-08 10:26:58 +0100

    ci: pin clusterfuzzlite tooling dependencies

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-go get github.com/AdamKorcz/go-118-fuzz-build/testing
+go get github.com/AdamKorcz/go-118-fuzz-build/testing@2b5cbb29f3e2e08ef2032ac4dc88a40a3a1e9e5f
 
 compile_native_go_fuzzer github.com/pandatix/go-cvss/20 FuzzParseVector fuzz_parse_vector_20
 compile_native_go_fuzzer github.com/pandatix/go-cvss/30 FuzzParseVector fuzz_parse_vector_30
